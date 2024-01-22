619b6b7a040169781a6f424dfa0deb6d5a8a67d8
xwings <kj@xandora.net>
2024-01-22 12:37:35 +0800

    feat: with GPU

--- btcwc.py
+++ btcwc.py
@@ -1,50 +1,45 @@
-import requests, sys
-from bit import Key
-from multiprocessing import cpu_count, Process
+import cupy as cp
+import requests
+import sys
+
+# Placeholder function for GPU-accelerated key generation
+def generate_and_check_keys_gpu(total_addresses, bwc):
+    while True:
+        # GPU-accelerated key generation
+        key, address = gpu_accelerated_key_generation()
+
+        # Check if the address is in the total_addresses list
+        if cp.any(total_addresses == address):
+            report = f"Found Public Address: {address} Private Key: {key}\n"
+            # Write report to file and handle the key
+            with open("found_keys.txt", "a") as file:
+                file.write(report)
+            # Send Telegram message if token and chat ID are set
+            if bwc.tg_token and bwc.tg_chat_id:
+                bwc.send_tg_msg(report)
 
 class Btcwc():
     def __init__(self):
-        self.debug      = True if len(sys.argv) > 1 and sys.argv[1] == "debug" else False
-        self.cpuhalf    = True if len(sys.argv) > 1 and sys.argv[1] == "half" else False
-        self.tg_token   = None
-        self.tg_chat_id = None
-        
+        self.debug = 'debug' in sys.argv
+        self.tg_token = None  # Set your Telegram token here
+        self.tg_chat_id = None  # Set your Telegram chat ID here
+
     def public_keys(self):
         load_data = open("address.csv", "r").readlines()
         load_data = [x.rstrip() for x in load_data]
-        # Remove invalid wallet addresses
-        load_data = [x for x in load_data if x.find('wallet') == -1 and len(x) > 0]
-        load_data  = dict(zip(load_data, load_data))
-        return load_data
+        load_data = [x for x in load_data if 'wallet' not in x and x]
+        return cp.array(load_data) # Convert to CuPy array
 
     def send_tg_msg(self, text):
-        url_req = "https://api.telegram.org/bot" + self.tg_token + "/sendMessage" + "?chat_id=" + self.tg_chat_id + "&text=" + text 
-        results = requests.get(url_req)
+        url_req = f"https://api.telegram.org/bot{self.tg_token}/sendMessage?chat_id={self.tg_chat_id}&text={text}" 
+        requests.get(url_req)
 
-    def random_brute(self):
-        key = Key()
-        def report():
-            return (f"Found Public Adress: {key.address} Private Key: {key.to_wif()}\n")
+    def letsroll(self, total_addresses):
+        generate_and_check_keys_gpu(total_addresses, self)
 
-        if key.address in self.total_address.keys():
-            with open("letsgetrich.txt", "a") as f:
-                f.write(report())
-            if (self.tg_token and self.tg_chat_id):
-                self.send_tg_msg(report())
-        elif self.debug is True:
-            print(report()) 
-            
-    def letsroll(self):
-        while True:
-            self.random_brute()
+if __name__ == "__main__":
+    bwc = Btcwc()
+    total_addresses = bwc.public_keys()
 
-if __name__ =="__main__":
-        bwc = Btcwc()
-        cpu_count = cpu_count() // 2 if bwc.cpuhalf is True else cpu_count()
-        bwc.total_address = bwc.public_keys()
-
-        print(f"\nTargeting {len(bwc.total_address.keys())} BTC addresses with {cpu_count} processes")
-        print(f"Benmarking: python3 {sys.argv[0]} debug | pv -i2 -ltr > /dev/null")
-        
-        for cpu in range(cpu_count):
-            Process(target = bwc.letsroll).start()
+    print(f"\nTargeting {len(total_addresses)} BTC addresses")
+    bwc.letsroll(total_addresses)
